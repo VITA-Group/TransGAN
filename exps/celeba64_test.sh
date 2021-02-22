ed9e80664d04f13807b084244c41e29083f510ca
Yifan <yifanjiang97@gmail.com>
2021-02-22 15:16:02 -0600

    celeba

---
+++ exps/celeba64_test.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+python test.py \
+-gen_bs 32 \
+-dis_bs 16 \
+--dataset stl10 \
+--bottom_width 8 \
+--img_size 64 \
+--max_iter 500000 \
+--gen_model Celeba64_TransGAN \
+--dis_model ViT_8_8 \
+--df_dim 384 \
+--d_depth 7 \
+--g_depth 5 \
+--latent_dim 1024 \
+--gf_dim 1024 \
+--num_workers 36 \
+--g_spectral_norm False \
+--d_spectral_norm True \
+--g_lr 0.0001 \
+--d_lr 0.0001 \
+--optimizer adam \
+--loss wgangp-eps \
+--wd 1e-3 \
+--beta1 0 \
+--beta2 0.99 \
+--phi 1 \
+--eval_batch_size 25 \
+--num_eval_imgs 50000 \
+--init_type xavier_uniform \
+--n_critic 5 \
+--val_freq 1 \
+--print_freq 50 \
+--grow_steps 0 0 \
+--fade_in 0 \
+--diff_aug translation,cutout,color \
+--load_path ./pretrained_weight/celeba64_checkpoint.pth \
+--exp_name celeba64_test
