a3d183b29f89fbc390cc4ba8c5cde60430a437d4
Yifan Jiang <14892792+yueruchen@users.noreply.github.com>
2021-06-21 00:37:27 -0500

    Update __init__.py

--- models_search/__init__.py
+++ models_search/__init__.py
@@ -1,1 +1,1 @@
-from models_search import ViT_custom_local544444_256_rp, ViT_scale3_local_new_rp, diff_aug, ada, ViT_helper, ViT_custom
+from models_search import ViT_custom_local544444_256_rp, ViT_scale3_local_new_rp, diff_aug, ada, ViT_helper, ViT_custom, ViT_custom_scale2
