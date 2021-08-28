30f39638cec417aaf8045be33e7502d464aea738
Yifan Jiang <14892792+yueruchen@users.noreply.github.com>
2021-08-28 11:30:56 -0400

    fix random seed

--- train_derived.py
+++ train_derived.py
@@ -35,10 +35,13 @@ def main():
 #     create_inception_graph(inception_path)
     
     if args.seed is not None:
+        torch.manual_seed(args.random_seed)
         torch.cuda.manual_seed(args.random_seed)
         torch.cuda.manual_seed_all(args.random_seed)
         np.random.seed(args.random_seed)
         random.seed(args.random_seed)
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
 
     if args.gpu is not None:
         warnings.warn('You have chosen a specific GPU. This will completely '
